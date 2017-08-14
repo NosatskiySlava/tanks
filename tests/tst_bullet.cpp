2f6c46a3ec50b765d63df39e022731d2ce855b80
Slava <nosatskyy.slava@gmail.com>
2017-08-14 23:08:39 +0200

    Added tests for bullet

---
+++ tests/tst_bullet.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+
+#include <Model/bullet.h>
+
+TEST(tst_bullet, moveUp) {
+   Bullet bullet(Position(10, 10), Common::EDirection::UP);
+   bullet.move();
+
+   ASSERT_EQ(bullet.x(), 10);
+   ASSERT_EQ(bullet.y(), 9);
+}
+
+TEST(tst_bullet, moveLeft) {
+   Bullet bullet(Position(10, 10), Common::EDirection::LEFT);
+   bullet.move();
+
+   ASSERT_EQ(bullet.x(), 9);
+   ASSERT_EQ(bullet.y(), 10);
+}
+
+TEST(tst_bullet, moveDown) {
+   Bullet bullet(Position(10, 10), Common::EDirection::DOWN);
+   bullet.move();
+
+   ASSERT_EQ(bullet.x(), 10);
+   ASSERT_EQ(bullet.y(), 11);
+}
+
+TEST(tst_bullet, moveRight) {
+   Bullet bullet(Position(10, 10), Common::EDirection::RIGHT);
+   bullet.move();
+
+   ASSERT_EQ(bullet.x(), 11);
+   ASSERT_EQ(bullet.y(), 10);
+}
