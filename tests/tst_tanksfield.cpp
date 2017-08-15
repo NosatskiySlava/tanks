f746f62ffbdbacd57a96a365e68c21c47df89a35
Viacheslav Nosatskyi <VNosatskyi@luxoft.com>
2017-08-15 18:32:46 +0200

    additional test for tanksField

--- tests/tst_tanksfield.cpp
+++ tests/tst_tanksfield.cpp
@@ -31,10 +31,23 @@ TEST(tst_tanksfiled, isHit) {
     std::shared_ptr<Bullet> bullet(new Bullet(Position(), Common::EDirection::UP));
     HitDetector hitDetector;
     QObject::connect(bullet.get(), SIGNAL(hit(void)), &hitDetector, SLOT(onHit(void)));
+
     field.addBullet(bullet);
     field.update();
 
     ASSERT_TRUE(hitDetector.isHit());
+}
+
+TEST(tst_tanksfiled, noHit) {
+    TanksField field;
+    std::shared_ptr<Bullet> bullet(new Bullet(Position(10, 10), Common::EDirection::UP));
+    HitDetector hitDetector;
+    QObject::connect(bullet.get(), SIGNAL(hit(void)), &hitDetector, SLOT(onHit(void)));
+
+    field.addBullet(bullet);
+    field.update();
+
+    ASSERT_FALSE(hitDetector.isHit());
 }
 
 #include <tst_tanksfield.moc>
